04057d2b48aa3f1a5f5976f249078f500b6fda96
XIAOKUOAIyang <1936786089@qq.com>
2020-10-17 12:14:57 +0800

    updata two

---
+++ build/precache-manifest.5e392ae88efcb8fec8d20ddc1f592ecb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d5a903ae222a1908de26edd41c3b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bce391348a026243b6ef",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "11b5272b42836ad76d51",
+    "url": "/static/css/main.18ff0a8f.chunk.css"
+  },
+  {
+    "revision": "bce391348a026243b6ef",
+    "url": "/static/js/2.714eb316.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.714eb316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b5272b42836ad76d51",
+    "url": "/static/js/main.f8972c74.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
