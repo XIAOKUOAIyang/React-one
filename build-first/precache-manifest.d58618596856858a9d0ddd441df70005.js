66ea20debdddb7611e42f5daa86823581d84d9c9
XIAOKUOAIyang <1936786089@qq.com>
2020-10-23 15:13:05 +0800

    commit first

---
+++ build-first/precache-manifest.d58618596856858a9d0ddd441df70005.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ec59400799a9cf5dc5d4b09a8c80bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52c223815ebe097e4d3d",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "4241fa9eee58668d2df0",
+    "url": "/static/css/main.8a7aea24.chunk.css"
+  },
+  {
+    "revision": "52c223815ebe097e4d3d",
+    "url": "/static/js/2.76caac25.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.76caac25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4241fa9eee58668d2df0",
+    "url": "/static/js/main.9d310bf9.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "454aec9a30e2ba55e1ff6c8d2f8ebd7d",
+    "url": "/static/media/list-four.454aec9a.webp"
+  },
+  {
+    "revision": "f2036db82acf1098fbd0ed786154f784",
+    "url": "/static/media/list-one.f2036db8.webp"
+  },
+  {
+    "revision": "33cd578d30b82ea28b0338e2490dd740",
+    "url": "/static/media/list-three.33cd578d.webp"
+  },
+  {
+    "revision": "65224a66cb966e78ab21aa26d78e3cb2",
+    "url": "/static/media/list-two.65224a66.webp"
+  },
+  {
+    "revision": "daaf4d60479fd0c22794c3a0adc1d780",
+    "url": "/static/media/lunbo-eight.daaf4d60.jpg"
+  },
+  {
+    "revision": "fb6da3f5586e6e031a175a454ccfc324",
+    "url": "/static/media/lunbo-five.fb6da3f5.jpg"
+  },
+  {
+    "revision": "1a95720dd9fc27c28ce0380e35fd4a9c",
+    "url": "/static/media/lunbo-four.1a95720d.jpg"
+  },
+  {
+    "revision": "a3ed787bd435f87a984f03580acd644e",
+    "url": "/static/media/lunbo-one.a3ed787b.jpg"
+  },
+  {
+    "revision": "71214b2b44ab8ae1314f0f89701366bd",
+    "url": "/static/media/lunbo-seven.71214b2b.jpg"
+  },
+  {
+    "revision": "adf7c28e5c51f339b984ae71edee0da1",
+    "url": "/static/media/lunbo-six.adf7c28e.jpg"
+  },
+  {
+    "revision": "88d5d6e82b095ffdd359b8a55fb2da2c",
+    "url": "/static/media/lunbo-three.88d5d6e8.jpg"
+  },
+  {
+    "revision": "47b7ac9d6f75ad6d150b0dca55a3fbdd",
+    "url": "/static/media/lunbo-two.47b7ac9d.jpg"
+  }
+]);
